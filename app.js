01ac686667f9d8eaec3ebd98dcff0c972be8b219
ALIAS GEORGE <aliasgeorge9329@gmail.com>
2022-06-12 14:50:11 +0530

    updated auth

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const qrcode = require("qrcode-terminal");
-const { Client, MessageMedia } = require("whatsapp-web.js");
+const { Client, MessageMedia, NoAuth } = require("whatsapp-web.js");
 const { exit } = require("process");
 const csv = require("csv-parser");
 const fs = require("fs");
@@ -21,38 +21,20 @@ const media = MessageMedia.fromFilePath("img.png");
 msg2 =
   "Exclusive benefits for you\n\n◇Internship vouchers on selected workshops\n◇Free exclusive NFTs on selected workshops\n◇Certificates for all participants\n\nFor further queries, please fill in: https://forms.gle/d3HJKNoUtPbS8myW7";
 
-// Path where the session data will be stored
-const SESSION_FILE_PATH = "./session.json";
-
-// Load the session data if it has been previously saved
-let sessionCfg;
-if (fs.existsSync(SESSION_FILE_PATH)) {
-  sessionCfg = require(SESSION_FILE_PATH);
-}
 
 // Use the saved values
 const client = new Client({
   puppeteer: { headless: false },
-  session: sessionCfg,
+  authStrategy: new NoAuth()
 });
 
 // const client = new Client({
 //   puppeteer: {
 //     browserWSEndpoint: `ws://localhost:3000`,
 //   },
-//   session: sessionCfg,
+//   authStrategy: new NoAuth()
 // });
 
-// Save session values to the file upon successful auth
-client.on("authenticated", (session) => {
-  console.log("youre in");
-  sessionData = session;
-  fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
-    if (err) {
-      console.error(err);
-    }
-  });
-});
 
 client.on("qr", (qr) => {
   qrcode.generate(qr, { small: true });
